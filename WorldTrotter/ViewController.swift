3afafde42803a01065b7aa5d60c0f76e14556980
Sujanth <sujanth1987@gmail.com>
2017-08-03 06:49:32 +1000

    World Trotter project started

---
+++ WorldTrotter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Sujanth Sebamalaithasan on 3/8/17.
+//  Copyright © 2017 Sujanth Sebamalaithasan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
